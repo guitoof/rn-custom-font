b636e138dd7b7f1c805f85576cd452d43cc30443
Guillaume Diallo-Mulliez <guillaumedm@bamlab.fr>
2017-02-25 15:51:01 +0100

    Add font usage for iOS

--- index.ios.js
+++ index.ios.js
@@ -16,15 +16,41 @@ export default class CustomFont extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.text}>
+          This is the default font
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+        <Text style={[styles.text, styles.customFont, styles.extraLight]}>
+          This is my awesome extra light font
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={[styles.text, styles.customFont, styles.light]}>
+          This is my awesome light font
+        </Text>
+        <Text style={[styles.text, styles.customFont]}>
+          This is my awesome font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.semiBold]}>
+          This is my awesome semi-bold font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.bold]}>
+          This is my awesome bold font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.black]}>
+          This is my awesome black font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.italic]}>
+          This is my awesome italic font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.extraLight, styles.italic]}>
+          This is my awesome extra light italic font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.light, styles.italic]}>
+          This is my awesome light italic font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.semiBold, styles.italic]}>
+          This is my awesome semi-bold italic font
+        </Text>
+        <Text style={[styles.text, styles.customFont, styles.italic, styles.bold]}>
+          This is my awesome bold italic font
         </Text>
       </View>
     );
@@ -38,15 +64,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  text: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  customFont: {
+    fontFamily: 'Titillium Web',
+  },
+  extraLight: {
+    fontWeight: '200'
+  },
+  light: {
+    fontWeight: '300'
+  },
+  semiBold: {
+    fontWeight: '600'
+  },
+  bold: {
+    fontWeight: '700'
+  },
+  black: {
+    fontWeight: '900'
+  },
+  italic: {
+    fontStyle: 'italic'
   },
 });
 
